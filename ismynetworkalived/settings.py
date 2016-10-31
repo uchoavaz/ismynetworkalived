8b96fe856e1fbec98d4bdc3c1c17b2993c577b38
uchoa <bioinfo@genomika.com.br>
2016-10-31 09:40:46 -0300

    config heroku

--- ismynetworkalived/settings.py
+++ ismynetworkalived/settings.py
@@ -26,9 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
